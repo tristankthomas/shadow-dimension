2cd5bc37b5ed6ea44bd416244528dad00f57c264
tkthomas <tristan.thomas@student.unimelb.edu.au>
2022-08-30 18:15:17 +1000

    Added the start menu

--- src/ShadowDimension.java
+++ src/ShadowDimension.java
@@ -1,10 +1,14 @@
 import bagel.*;
-
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 /**
  * Skeleton Code for SWEN20003 Project 1, Semester 2, 2022
  *
  * Please enter your name below
- * @author
+ * @author Tristan Thomas
  */
 
 public class ShadowDimension extends AbstractGame {
@@ -12,6 +16,9 @@ public class ShadowDimension extends AbstractGame {
     private final static int WINDOW_HEIGHT = 768;
     private final static String GAME_TITLE = "SHADOW DIMENSION";
     private final Image BACKGROUND_IMAGE = new Image("res/background0.png");
+    private final Font DEFAULT_FONT = new Font("res/frostbite.ttf", 75);
+    private final Font INSTRUCTION_FONT = new Font("res/frostbite.ttf", 40);
+    private boolean gameStart = false;
 
     public ShadowDimension(){
         super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
@@ -31,6 +38,7 @@ public class ShadowDimension extends AbstractGame {
      */
     private void readCSV(){
 
+
     }
 
     /**
@@ -39,10 +47,19 @@ public class ShadowDimension extends AbstractGame {
      */
     @Override
     protected void update(Input input) {
-        BACKGROUND_IMAGE.draw(Window.getWidth()/2.0, Window.getHeight()/2.0);
 
-        if (input.wasPressed(Keys.ESCAPE)){
+
+        if (input.wasPressed(Keys.ESCAPE))
             Window.close();
+        else if (!gameStart) {
+            if (input.wasPressed(Keys.SPACE))
+                gameStart = true;
+            DEFAULT_FONT.drawString(GAME_TITLE, 260, 250);
+            INSTRUCTION_FONT.drawString("PRESS SPACE TO START", 350, 440);
+            INSTRUCTION_FONT.drawString("USE ARROW KEYS TO FIND GATE", 350, 490);
+        } else {
+            BACKGROUND_IMAGE.draw(Window.getWidth() / 2.0, Window.getHeight() / 2.0);
+
         }
 
     }
